4db2eac6ca0630252313d46852fd2f76a5878bd0
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-19 08:35:50 +0100

    test: App
    Updates App tests to include BrowserRouter component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "../components/App";
 
 describe("App", () => {
   it("renders the nav bar", () => {
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
     const navBarEl = screen.getByTestId("navbar");
 
     expect(navBarEl).toBeInTheDocument();
