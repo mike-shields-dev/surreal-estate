21a4b817dd2854e6c63720a73e474bdf794d9ab4
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-19 08:51:18 +0100

    test: App
    Updates tests to assert client side routing logic

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "../components/App";
 
 describe("App", () => {
-  it("renders the nav bar", () => {
+  it("renders NavBar", () => {
     render(
       <BrowserRouter>
         <App />
@@ -13,5 +13,35 @@ describe("App", () => {
     const navBarEl = screen.getByTestId("navbar");
 
     expect(navBarEl).toBeInTheDocument();
+  });
+
+  it("clicking the 'View Properties' link, renders the Properties component", async () => {
+    const { getByRole } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    const linkEl = getByRole("link", { name: "View Properties" });
+
+    expect(screen.queryByText("Properties Page")).not.toBeInTheDocument();
+
+    fireEvent.click(linkEl);
+
+    expect(screen.queryByText("Properties Page")).toBeInTheDocument();
+  });
+
+  it("clicking the 'Add Property' link, renders the AddProperty component", async () => {
+    const { getByRole } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    const linkEl = getByRole("link", { name: "Add Property" });
+
+    expect(screen.queryByText("Add Property Page")).not.toBeInTheDocument();
+
+    fireEvent.click(linkEl);
+
+    expect(screen.queryByText("Add Property Page")).toBeInTheDocument();
   });
 });
