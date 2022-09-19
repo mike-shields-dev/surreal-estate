3c3cb2f0dbc63a914eac42058b6155cdd1be2977
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-19 08:35:15 +0100

    Client side routing
    - Imports Switch and Route components from react-router-dom
    - Imports Properties and AddProperties components
    - Implements client side Routing of Properties and AddProperties components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "../styles/app.css";
+import AddProperty from "./AddProperty";
 import NavBar from "./NavBar";
+import Properties from "./Properties";
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
       <h2>Surreal Estate</h2>
+      <Switch>
+        <Route path="/properties">
+          <Properties />
+        </Route>
+        <Route path="/add-property">
+          <AddProperty />
+        </Route>
+      </Switch>
     </div>
   );
 };
