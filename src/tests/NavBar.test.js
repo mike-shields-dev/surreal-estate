c365cd54fd4e917f8c8bb7a5e2988261943d62a4
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-09-19 08:24:02 +0100

    test: NavBar
    Adds tests for NavBar

--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -1,30 +1,41 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import NavBar from "../components/NavBar";
+
+const NavBarWithBrowserRouter = () => (
+  <BrowserRouter>
+    <NavBar />
+  </BrowserRouter>
+);
 
 describe("Navbar", () => {
   it("matches snapshot", () => {
-    const { asFragment } = render(<NavBar />);
+    const { asFragment } = render(<NavBarWithBrowserRouter />);
 
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders the company logo", () => {
-    const { getByTestId } = render(<NavBar />);
+    const { getByTestId } = render(<NavBarWithBrowserRouter />);
     const logoEl = getByTestId("company-logo");
 
     expect(logoEl).toBeInTheDocument();
   });
 
-  it("renders a 'View Properties' hyperlink", () => {
-    const { getByText } = render(<NavBar />);
+  it("renders a 'View Properties' hyperlink with a href of '/properties'", () => {
+    const { getByRole } = render(<NavBarWithBrowserRouter />);
+    const linkEl = getByRole("link", { name: "View Properties" });
 
-    expect(getByText("View Properties")).toBeInTheDocument();
+    expect(linkEl).toBeInTheDocument();
+    expect(linkEl).toHaveAttribute("href", "/properties");
   });
 
-  it("renders an 'Add Property' hyperlink", () => {
-    const { getByText } = render(<NavBar />);
+  it("renders an 'Add Property' hyperlink with a href of '/add-property'", () => {
+    const { getByRole } = render(<NavBarWithBrowserRouter />);
+    const linkEl = getByRole("link", { name: "Add Property" });
 
-    expect(getByText("Add Property")).toBeInTheDocument();
+    expect(linkEl).toBeInTheDocument();
+    expect(linkEl).toHaveAttribute("href", "/add-property");
   });
 });
