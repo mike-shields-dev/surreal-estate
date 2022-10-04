1da3198565ec124532fb42039dbe069289a5ee31
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-10-04 13:51:48 +0100

    refactor: NavBar
    Replaces brand img with FontAwesome icon and real text

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FaFortAwesome } from "react-icons/fa";
 import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
     <nav className={styles.navbar} data-testid="navbar">
-      <img
-        className={styles.navbar__logo}
-        data-testid="company-logo"
-        src="/surreal-logo.png"
-        alt="surreal estate logo"
-      />
+      <div className={styles.navbar__brand}>
+        <FaFortAwesome
+          className={styles["navbar__brand-logo"]}
+          title="surreal estate logo"
+        />
+        <h1 className={styles["navbar__brand-name"]}>Surreal Estate</h1>
+      </div>
       <ul className={styles["navbar__links-list"]}>
         <li className={styles["navbar__links-list-item"]}>
           <NavLink
