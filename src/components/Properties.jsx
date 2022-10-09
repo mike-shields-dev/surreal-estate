1a01926dd12dc07d1244e55bf242ece27bdad5ba
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-10-09 13:21:55 +0100

    refactor: Properties
    Applies useAPI hook to component

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -1,46 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import axios from "axios";
 import PropertyCard from "./PropertyCard";
 import Alert from "./Alert";
 import SideBar from "./SideBar";
 import styles from "../styles/Properties.module.css";
-import getProperties from "../requests/getProperties";
+import useAPI from "../requests/useAPI";
 import cities from "../config/cities.json";
 
-const Properties = () => {
-  const [properties, setProperties] = useState([]);
-  const [alert, setAlert] = useState({
+const initialState = {
+  alert: {
     message: "",
     isSuccess: false,
-  });
-  const { pathname, search } = useLocation();
+  },
+};
+
+const Properties = () => {
+  const { search } = useLocation();
+  const { request, error, response, controller } = useAPI();
+  const [properties, setProperties] = useState([]);
+  const [alert, setAlert] = useState(initialState.alert);
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
 
   useEffect(() => {
-    const source = axios.CancelToken.source();
-    (async () => {
-      try {
-        const { data } = await getProperties({ search, source });
-        setProperties(data);
-        setAlert({ message: "", isSuccess: true });
-      } catch (err) {
-        setAlert({
-          message: "Something went wrong, please try again later",
-          isSuccess: false,
-        });
-      }
-    })();
-    return () => {
-      source.cancel();
-    };
+    request({ method: "get", search });
   }, [search]);
+
+  useEffect(() => {
+    let { message, isSuccess } = initialState.alert;
+
+    if (error) {
+      message = "Network error, please check your internet connection.";
+      isSuccess = false;
+    }
+    if (response) {
+      if (response.status === 200) {
+        setProperties(response.data);
+      }
+    }
+    setAlert({ message, isSuccess });
+  }, [response, error]);
+
+  useEffect(() => controller?.abort(), []);
 
   return (
     <>
-      <SideBar
-        {...{ pathname, search, cities, isSideBarOpen, setIsSideBarOpen }}
-      />
+      <SideBar {...{ cities, isSideBarOpen, setIsSideBarOpen }} />
       <div className={styles.properties} title="properties">
         <Alert {...alert} />
         <div className={styles.properties__grid}>
