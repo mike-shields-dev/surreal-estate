a67aabc5c4a16e9bc90aca81b036a3fd17ff63d2
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-10-17 17:13:42 +0100

    refactor: Properties
    Removes useAPI hook

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -2,105 +2,36 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useLocation } from "react-router-dom";
 import PropertyCard from "./PropertyCard";
-import Alert from "./Alert";
 import SideBar from "./SideBar";
 import css from "../styles/Properties.module.css";
-import useAPI from "../hooks/useAPI";
+import { getProperties, getFavourites } from "../requests/API";
 import cities from "../config/cities.json";
-
-const initialState = {
-  alert: {
-    message: "",
-    isSuccess: false,
-  },
-};
 
 const Properties = ({ userId }) => {
   const { search } = useLocation();
   const [properties, setProperties] = useState([]);
   const [favourites, setFavourites] = useState([]);
-  const [alert, setAlert] = useState(initialState.alert);
-  const {
-    request: favouriteRequest,
-    error: favouriteError,
-    response: favouriteResponse,
-    controller: favouriteController,
-  } = useAPI();
-  const {
-    request: propertiesRequest,
-    error: propertiesError,
-    response: propertiesResponse,
-    controller: propertiesController,
-  } = useAPI();
-
-  const requestFavourites = () => {
-    favouriteRequest({
-      method: "get",
-      endpoint: "/Favourite",
-      params: `?query={"fbUserId":"${userId}"}`,
-    });
-  };
-
-  const requestProperties = () => {
-    propertiesRequest({
-      method: "get",
-      params: search,
-      endpoint: "/PropertyListing",
-    });
-  };
-
-  useEffect(() => requestProperties(), [search]);
-  useEffect(() => requestFavourites(), [userId]);
 
   useEffect(() => {
-    let { message, isSuccess } = initialState.alert;
-
-    if (propertiesError) message = propertiesError.message;
-    if (propertiesResponse?.status === 200) {
-      setProperties(propertiesResponse.data);
-      isSuccess = true;
-    }
-    setAlert({ message, isSuccess });
-  }, [propertiesResponse, propertiesError]);
+    (async () => setProperties(await getProperties(search)))();
+  }, [search]);
 
   useEffect(() => {
-    let { message, isSuccess } = initialState.alert;
-
-    if (favouriteError) message = favouriteError.message;
-    if (favouriteResponse?.status === 200) {
-      const uniqueFavourites = favouriteResponse.data.reduce((array, cur) => {
-        if (!array.some((el) => el.propertyListing === cur.propertyListing)) {
-          return [...array, cur];
-        }
-        return array;
-      }, []);
-      setFavourites(uniqueFavourites);
-      isSuccess = true;
-    }
-    setAlert({ message, isSuccess });
-  }, [favouriteResponse, favouriteError]);
-
-  useEffect(
-    () => () => {
-      favouriteController?.abort();
-      propertiesController?.abort();
-    },
-    []
-  );
+    (async () => setFavourites(await getFavourites(userId)))();
+  }, [userId]);
 
   return (
     <>
       <SideBar {...{ cities }} />
       <div className={css.properties} title="properties">
-        <Alert {...alert} />
         <div className={css.properties__grid}>
           {properties.map((property) => (
             <PropertyCard
               favourites={favourites}
               key={property._id}
               property={property}
-              requestFavourites={requestFavourites}
               userId={userId}
+              setFavourites={setFavourites}
             />
           ))}
         </div>
