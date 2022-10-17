1c71de6b56aca465fd873cf45610fa3545add62c
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-10-17 22:18:18 +0100

    style: GoogleSingleSignOn
    Add custom button

--- src/components/GoogleSingleSignOn.jsx
+++ src/components/GoogleSingleSignOn.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { gapi } from "gapi-script";
+import { FcGoogle } from "react-icons/fc";
 import { GoogleLogin, GoogleLogout } from "react-google-login";
 import css from "../styles/GoogleSingleSignOn.module.css";
 import credentials from "../config/googleCredentials.json";
@@ -33,17 +34,27 @@ const GoogleSingleSignOn = ({ username, setUserProfile }) => {
 
   return (
     <div className={css.GoogleSingleSignOn}>
-      {username && <div>Welcome, {username}</div>}
+      {username && <p>Welcome, {username}</p>}
       {username ? (
         <GoogleLogout
           clientId={clientId}
-          buttonText="Logout"
           onLogoutSuccess={onLogoutSuccess}
+          render={({ onClick }) => (
+            <button className={css.button} type="button" onClick={onClick}>
+              <FcGoogle />
+              Logout
+            </button>
+          )}
         />
       ) : (
         <GoogleLogin
           clientId={clientId}
-          buttonText="Login"
+          render={({ onClick }) => (
+            <button className={css.button} type="button" onClick={onClick}>
+              <FcGoogle />
+              Login
+            </button>
+          )}
           onSuccess={onLoginSuccess}
           onFailure={onLoginFailure}
           cookiePolicy="single_host_origin"
